9867e5589a9e797165268a3b274838023abf596e
fachryhidayat7 <fachryhidayat7@gmail.com>
2020-02-20 11:01:52 +0800

    menyiapkan template admin

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,9 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('admin.templates.default');
+});
+
+Route::get('/user', function() {
+    return view('admin.users.index');
 });
