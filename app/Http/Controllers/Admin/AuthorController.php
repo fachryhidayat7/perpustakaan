db1e2ba03999e501dca90c096acf5fae23c02700
fachryhidayat7 <fachryhidayat7@gmail.com>
2020-02-22 11:58:17 +0800

    menambah breadcrumbs laravel

--- app/Http/Controllers/Admin/AuthorController.php
+++ app/Http/Controllers/Admin/AuthorController.php
@@ -15,7 +15,9 @@ class AuthorController extends Controller
      */
     public function index()
     {
-        return view('admin.author.index');
+        return view('admin.author.index',[
+            'title' => 'Data Penulis',
+        ]);
     }
 
     /**
@@ -25,7 +27,9 @@ class AuthorController extends Controller
      */
     public function create()
     {
-        return view('admin.author.create');
+        return view('admin.author.create',[
+            'title' => 'Tambah Penulis'
+        ]);
     }
 
     /**
@@ -61,6 +65,7 @@ class AuthorController extends Controller
     public function edit(Author $author)
     {
         return view('admin.author.edit',[
+            'title'  => 'Edit Penulis',
             'author' => $author,
         ]);
     }
--- resources/views/admin/templates/default.blade.php
+++ resources/views/admin/templates/default.blade.php
@@ -12,13 +12,14 @@
   <div class="content-wrapper">
     <section class="content-header">
         <h1>
-          Dashboard
-          <small>Control panel</small>
+          {{ Breadcrumbs::current()->title }}
         </h1>
-        <ol class="breadcrumb">
+        {{ Breadcrumbs::render() }}
+
+        {{-- <ol class="breadcrumb">
           <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
           <li class="active">Dashboard</li>
-        </ol>
+        </ol> --}}
       </section>
     <!-- Main content -->
     <section class="content">
